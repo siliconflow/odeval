3b7db696376a1f191c9a0dd9e294665ae0f61b97
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2024-03-25 14:04:15 +0800

    Update run_sdxl_tests.sh

--- run_sdxl_tests.sh
+++ run_sdxl_tests.sh
@@ -5,10 +5,10 @@ export HF_ENDPOINT="https://hf-mirror.com"
 
 MODEL_PATH=$(python3 -c "from huggingface_hub import snapshot_download; repo_id='siliconflow/stable-video-diffusion-img2vid-xt-deepcache-int8'; deepcache_int8_folder = snapshot_download(repo_id=repo_id); print(deepcache_int8_folder)")
 
-OUTPUT_DIR="/home/lixiang/eval/eval5"
+OUTPUT_DIR="your path"
 
-# python3 text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR
-# python3 text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR --deep_cache False
-# python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR
-# python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --deep_cache False
+python3 text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR
+python3 text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR --deep_cache False
+python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR
+python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --deep_cache False
 python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --compile False --deep_cache False
