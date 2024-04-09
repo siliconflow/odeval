e1836b6d92b00105f8553d30408c0d565a9110f1
lixiang007666 <88304454@qq.com>
2024-04-09 21:46:32 +0800

    Refactor

--- run_sdxl_tests.sh
+++ run_sdxl_tests.sh
@@ -7,8 +7,8 @@ MODEL_PATH=$(python3 -c "from huggingface_hub import snapshot_download; repo_id=
 
 OUTPUT_DIR="your path"
 
-python3 text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR
-python3 text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR --deep_cache False
-python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR
-python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --deep_cache False
-python3 text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --compile False --deep_cache False
+python3 sdxl/text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR
+python3 sdxl/text_to_image_sdxl_enterprise_quality_benchmark.py --model $MODEL_PATH --image_path $OUTPUT_DIR --deep_cache False
+python3 sdxl/text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR
+python3 sdxl/text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --deep_cache False
+python3 sdxl/text_to_image_sdxl_quality_benchmark.py --image_path $OUTPUT_DIR --compile False --deep_cache False
